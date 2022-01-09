12b960df6f45318c8c12f54e64d89f4ef92852c8
fsk <furkankulaksiz68@gmail.com>
2022-01-09 22:16:59 +0300

    heroku deploydan sonraki ilk degisiklikler

--- src/main/java/com/fsk/datapreprocessingtechniques/controller/FirstPreprocessing.java
+++ src/main/java/com/fsk/datapreprocessingtechniques/controller/FirstPreprocessing.java
@@ -24,13 +24,13 @@ public class FirstPreprocessing {
                 <strong>Kaynak Kod:</strong><a href="https://github.com/fsk" target="_blank">Kaynak Kod için bu yazının üstüne tıklayınız</a>
                 <hr>
                 <br>
-                <strong><i>Hocam öncelikle ödevi geç göndermeme müsade ettiğiniz için çok teşekkür ederim.</i></strong>
+                <p style="font-size: 32px"><strong><i>Hocam öncelikle ödevi geç göndermeme müsade ettiğiniz için çok teşekkür ederim.</i></strong>
                 <strong>Çalıştığım firmada yurtdışı projesi için demo halindeyiz ve sürekli İstanbul - Ankara arası mekik dokumak durumundayım.</strong>
                 <strong>İnanın bu ödevin başına da Anca bu haftasonu oturabildim. Cuma günü oturdum. Bu satırları yazarken Pazar saat 21.43</strong>
                 <strong>Lisansta bitirme projemde de sizin kurulunuzdaydım. Hatırlarsanız PSO ile sudoku çözdürmüştüm. Normalde gerçekten böyle bir şey yapmazdım
                 fakat maalesef bu ödev için böyle oldu. Eksik olan bir kaç nokta var fakat hem yetiştiremeyeceğim sanırım daha fazla. Hem de veri ön işleme tekniklerini
                 sizin dersiniz olduğu için aldım. Yoksa sektörde benim çok fazla işime yaramayacak bir ders. Yani dersi geçsem bana yeter. Bu notları yazarken de kendimi
-                sınav kağıtlarına not yazan öğrenciler gibi hissettim. ((: Neyse Hocam, sizlere iyi calismalar.</strong>
+                sınav kağıtlarına not yazan öğrenciler gibi hissettim. ((: Neyse Hocam, sizlere iyi calismalar.</strong></p>
                 <br>
                 <hr>
                 <br>
@@ -48,7 +48,7 @@ public class FirstPreprocessing {
                 <br>
                 <br>
                 <button>
-                    <a href="http://localhost:8080/data-preprocessor-techniques/">UYGULAMA</a>
+                    <a href="https://data-preporessor-techniques.herokuapp.com/data-preprocessor-techniques/">UYGULAMA</a>
                 </button>
                                 
                 </body>
